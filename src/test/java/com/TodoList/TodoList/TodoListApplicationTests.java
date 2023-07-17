e5896d18fe49767c0c2bfde73399289eb87b8049
zensen6 <junhee1469@gmail.com>
2023-07-17 23:10:12 +0900

    yml-db set

---
+++ src/test/java/com/TodoList/TodoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TodoList.TodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
